167a88d12b3f1e6958418ceb686314cd1c8d148d
iancwe <crubbishhute@gmail.com>
2017-06-09 13:25:31 +0800

    changing some stuff

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,7 +14,7 @@ class About extends Component {
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               <a href='mailto:crubbishhute@gmail.com'><i className='fa fa-envelope-o fa-3x' aria-hidden='true' /></a>
               <a href='https://github.com/iancwe'><i className='fa fa-github-square fa-3x' aria-hidden='true' /></a>
-              <a href='#'><i className='fa fa-linkedin-square fa-3x' aria-hidden='true' /></a>
+              <a href='https://www.linkedin.com/in/ian-c-064734144/'><i className='fa fa-linkedin-square fa-3x' aria-hidden='true' /></a>
             </Col>
             <Col md={6}>
               <a href='https://github.com/iancwe'><img src={require('../images/profilepic2.png')} alt='profile picture' id='pp' /></a>
